fd7de1b783969722bd6a96ba3e375c5323c68295
Noah Berry <noahmb409@gmail.com>
2026-02-16 11:26:01 -0600

    BaseTask

---
+++ BaseTask.cs
@@ -0,0 +1,21 @@
+namespace TaskManagerPro;
+
+public abstract class BaseTask : ITask{
+    public string Title {get; protected set; }
+    public bool IsCompleted {get; protected set; }
+
+    public BaseTask(string title)
+    {
+        
+        Title = title;
+    }
+
+    public virtual void Complete()
+    {
+        IsCompleted = true;
+    }
+
+    public abstract virtual Display();
+}
+
+
